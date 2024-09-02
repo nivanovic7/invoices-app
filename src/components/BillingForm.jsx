dc1ed388cc61844510f34d0673993dcc011722d0
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2024-09-02 23:09:15 +0200

    Implemented formik  initial values into Edit form

--- src/components/BillingForm.jsx
+++ src/components/BillingForm.jsx
@@ -5,35 +5,60 @@ import Heading from "./Heading";
 import { Formik, Form } from "formik";
 import ItemList from "./ItemList";
 import Button from "./Button";
+import { useInvoices } from "../contexts/InvoiceContext";
+
+// id: uuidv4(),
+// status: "draft",
+// date: "Aug 18 2023",
+// paymentTerms: "Net 30 days",
+// sender: {
+//   street: "cara dusana",
+//   city: "banjaluka",
+//   postCode: 78000,
+//   country: "BiH",
+// },
+// client: {
+//   name: "nikola invaonvic",
+//   email: "nikola@gmial.com",
+//   street: "cara lazara",
+//   city: "nevesinje",
+//   postCode: 88289,
+//   country: "BiH",
+// },
+// projectDescription: "It job",
 
 function BillingForm() {
-  console.log(new Date());
+  const { selectedInvoice, dispatch } = useInvoices();
   return (
     <Formik
       className={styles.form}
-      initialValues={{ date: null }}
+      initialValues={selectedInvoice}
       onSubmit={(values) => console.log(values.date)}
     >
       <Form className={styles.form}>
         <Heading text="Create Invoice" />
         <h3>Bill from</h3>
-        <TextInput label="Street Address" name="street" type="text" />
+        <TextInput label="Street Address" name="sender.street" type="text" />
         <div className={styles.formGroup}>
-          <TextInput label="City" name="city" type="text" />
-          <TextInput label="Post Code" name="postCode" type="text" />
-          <TextInput label="Country" name="country" type="text" />
+          <TextInput label="City" name="sender.city" type="text" />
+          <TextInput label="Post Code" name="sender.postCode" type="text" />
+          <TextInput label="Country" name="sender.country" type="text" />
         </div>
 
         <h3>Bill to</h3>
-        <TextInput label="Clinet's Name" name="clientStreet" type="text" />
-        <TextInput label="Client's Email" name="clientEmail" type="text" />
-        <TextInput label="Street Address" name="clientAddress" type="text" />
+        <TextInput label="Clinet's Name" name="sender.street" type="text" />
+        <TextInput label="Client's Email" name="sender.email" type="text" />
+        <TextInput label="Street Address" name="sender.address" type="text" />
         <div className={styles.formGroup}>
-          <TextInput label="City" name="clientCity" type="text" />
-          <TextInput label="Post Code" name="ClientPostCode" type="text" />
-          <TextInput label="Country" name="clientCountry" type="text" />
+          <TextInput label="City" name="client.city" type="text" />
+          <TextInput label="Post Code" name="client.postCode" type="text" />
+          <TextInput label="Country" name="client.country" type="text" />
         </div>
-        <TextInput label="Project desctiption" name="projectDesc" type="text" />
+        <TextInput
+          label="Project desctiption"
+          name="projectDescription"
+          type="text"
+        />
         <ItemList />
 
         <div className={styles.btnWrap}>
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,8 +1,16 @@
+import { FieldArray, useFormikContext } from "formik";
 import Button from "./Button";
 import styles from "./ItemList.module.css";
 import TextInput from "./TextInput";
 
+// items: [
+//   { itemName: "Laptop", quantity: 1, price: 999 },
+//   { itemName: "Iphone", quantity: 2, price: 1550 },
+// ],
+
 function ItemList() {
+  const { values } = useFormikContext();
+
   return (
     <>
       <div className={styles.itemList}>
@@ -11,18 +19,35 @@ function ItemList() {
         <span>Price</span>
         <span>Total</span>
         <span className="hidden"></span>
-        <TextInput name="x" />
-        <TextInput name="q" />
-        <TextInput name="t" />
-        <span>156</span>
-        <span>
-          <svg width="25px" height="25px" viewBox="0 0 24 24">
-            <path d="M5.755,20.283,4,8H20L18.245,20.283A2,2,0,0,1,16.265,22H7.735A2,2,0,0,1,5.755,20.283ZM21,4H16V3a1,1,0,0,0-1-1H9A1,1,0,0,0,8,3V4H3A1,1,0,0,0,3,6H21a1,1,0,0,0,0-2Z" />
-          </svg>
-        </span>
+        <FieldArray name="items">
+          {({ push, remove }) => {
+            return (
+              <>
+                {values.items.map((_, index) => (
+                  <>
+                    <TextInput name={`items.${index}.itemName`} />
+                    <TextInput name={`items.${index}.quantity`} />
+                    <TextInput name={`items.${index}.price`} />
+                    <span>156</span>
+                    <span onClick={() => remove(index)}>
+                      <svg width="25px" height="25px" viewBox="0 0 24 24">
+                        <path d="M5.755,20.283,4,8H20L18.245,20.283A2,2,0,0,1,16.265,22H7.735A2,2,0,0,1,5.755,20.283ZM21,4H16V3a1,1,0,0,0-1-1H9A1,1,0,0,0,8,3V4H3A1,1,0,0,0,3,6H21a1,1,0,0,0,0-2Z" />
+                      </svg>
+                    </span>
+                  </>
+                ))}
+                <Button
+                  handleClick={() =>
+                    push({ itemName: "", quantity: "", price: "" })
+                  }
+                  colorClass="btnSecondary"
+                  text="+ Add new item"
+                />
+              </>
+            );
+          }}
+        </FieldArray>
       </div>
-
-      <Button colorClass="btnSecondary" text="+ Add new item" />
     </>
   );
 }
